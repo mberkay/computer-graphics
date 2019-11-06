13a8cb449c141d9f8fbd5ce306450c040d830021
Berkay Ozkan <mberkayo@gmail.com>
2019-11-06 20:48:38 +0300

    Implement Mesh

---
+++ mesh.py
@@ -0,0 +1,21 @@
+# CENG 487 Assignment1 by
+# Mustafa Berkay Özkan
+# StudentId: 230201005
+# 11 2019
+
+import random
+
+from vec3d import Vec3d
+
+
+class Mesh:
+    def __init__(self, vertices, triangles):
+        self.vertices = vertices
+        self.triangles = triangles
+        self.color = []
+        self.create_colors()
+        self.can_subdivide = False
+
+    def create_colors(self):
+        for i in range(len(self.triangles)):
+            self.color.append(Vec3d(random.randint(0,255),random.randint(0,255),random.randint(0,255)) / 255)
